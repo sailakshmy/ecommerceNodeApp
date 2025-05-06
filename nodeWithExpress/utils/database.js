fa5417591d7dd29d92da11a6ebbbbbc6c43bf084
Sai <sailakshmy94@gmail.com>
2025-05-06 06:24:21 +0400

    Installed sequelize and connected it to the mysql DB

--- nodeWithExpress/utils/database.js
+++ nodeWithExpress/utils/database.js
@@ -1,10 +1,18 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node-complete",
+//   password: "IAmGroot123",
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("node-complete", "root", "IAmGroot123", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "IAmGroot123",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
