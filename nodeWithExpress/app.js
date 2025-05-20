4c21c8effca53caaeafe3bbccaba806d08db52ab
Sai <sailakshmy94@gmail.com>
2025-05-20 07:08:00 +0400

    Created the MongoClient instance and connected the app to MongoDB

--- nodeWithExpress/app.js
+++ nodeWithExpress/app.js
@@ -5,24 +5,18 @@ const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
+const mongoConnect = require("./utils/database");
+
 // const db = require("./utils/database");
-const sequelize = require("./utils/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+// const sequelize = require("./utils/database");
+// const Product = require("./models/product");
+// const User = require("./models/user");
+// const Cart = require("./models/cart");
+// const CartItem = require("./models/cart-item");
+// const Order = require("./models/order");
+// const OrderItem = require("./models/order-item");
 
 const app = express();
-
-// db.execute("SELECT * FROM  products")
-//   .then((res) => {
-//     console.log("res", res[0], res[1]);
-//   })
-//   .catch((err) => {
-//     console.log("err", err);
-//   });
 
 app.set("view engine", "ejs");
 
@@ -33,12 +27,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
@@ -46,55 +40,60 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// Sequelize Associations
-Product.belongsTo(User, {
-  constraints: true,
-  onDelete: "CASCADE",
+mongoConnect((client) => {
+  console.log("client", client);
+  app.listen(3000);
 });
-// A user can have many products
-User.hasMany(Product);
 
-// A user can have one cart
-User.hasOne(Cart);
-Cart.belongsTo(User);
+// // Sequelize Associations
+// Product.belongsTo(User, {
+//   constraints: true,
+//   onDelete: "CASCADE",
+// });
+// // A user can have many products
+// User.hasMany(Product);
 
-// A Cart can belong to many Products
-Cart.belongsToMany(Product, { through: CartItem });
+// // A user can have one cart
+// User.hasOne(Cart);
+// Cart.belongsTo(User);
 
-// A product can belong to multiple carts
-Product.belongsToMany(Cart, { through: CartItem });
+// // A Cart can belong to many Products
+// Cart.belongsToMany(Product, { through: CartItem });
 
-// A user can have many orders
-User.hasMany(Order);
-Order.belongsTo(User);
+// // A product can belong to multiple carts
+// Product.belongsToMany(Cart, { through: CartItem });
 
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
+// // A user can have many orders
+// User.hasMany(Order);
+// Order.belongsTo(User);
 
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    // console.log("res", result);
-    // app.listen(3000);
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    console.log("user before", user);
-    if (!user) {
-      // Creating a dummy user
-      return User.create({ name: "abc", email: "abc@test.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    // Creating a dummy cart
-    return user.createCart();
-    // console.log("user", user);
-  })
-  .then(() => {
-    app.listen(3000);
-  })
-  .catch((e) => {
-    console.log("e", e);
-  });
+// Order.belongsToMany(Product, { through: OrderItem });
+// Product.belongsToMany(Order, { through: OrderItem });
+
+// sequelize
+//   // .sync({ force: true })
+//   .sync()
+//   .then((result) => {
+//     // console.log("res", result);
+//     // app.listen(3000);
+//     return User.findByPk(1);
+//   })
+//   .then((user) => {
+//     console.log("user before", user);
+//     if (!user) {
+//       // Creating a dummy user
+//       return User.create({ name: "abc", email: "abc@test.com" });
+//     }
+//     return user;
+//   })
+//   .then((user) => {
+//     // Creating a dummy cart
+//     return user.createCart();
+//     // console.log("user", user);
+//   })
+//   .then(() => {
+//     app.listen(3000);
+//   })
+//   .catch((e) => {
+//     console.log("e", e);
+//   });
--- nodeWithExpress/utils/database.js
+++ nodeWithExpress/utils/database.js
@@ -9,10 +9,28 @@
 
 // module.exports = pool.promise();
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("node-complete", "root", "IAmGroot123", {
-  dialect: "mysql",
-  host: "localhost",
-});
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize("node-complete", "root", "IAmGroot123", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+const uri =
+  "mongodb+srv://Groot:IAmGroot@cluster0.2ehxgue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(uri)
+    .then((client) => {
+      console.log("Connected!!!");
+      callback(client);
+    })
+    .catch((e) => console.log("error in connecting to MongoDb", e));
+};
+
+module.exports = mongoConnect;
