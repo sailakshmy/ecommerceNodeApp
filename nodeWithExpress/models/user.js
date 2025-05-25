3044faea63eb5cbe5ef36799047c28ac48414ae0
Sai <sailakshmy94@gmail.com>
2025-05-25 12:06:47 +0400

    Added the AddToCart method in the user model itself

--- nodeWithExpress/models/user.js
+++ nodeWithExpress/models/user.js
@@ -2,13 +2,30 @@ const getDb = require("../utils/database").getDb;
 const mongodb = require("mongodb");
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items:[]}
+    this._id = id;
   }
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const db = getDb();
+    const cartProduct = this.cart.items.findIndex((cartItem) => {
+      return product?._id === cartItem?._id;
+    });
+    // product.quantity = 1;
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
