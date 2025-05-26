be2af202d70029ee24672bf2bdbe78037e2345db
Sai <sailakshmy94@gmail.com>
2025-05-26 07:24:39 +0400

    Fetched a single product

--- nodeWithExpress/controllers/shop.js
+++ nodeWithExpress/controllers/shop.js
@@ -18,6 +18,7 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
 
+  // Product.findById(prodId)
   Product.findById(prodId)
     .then((product) => {
       res.render("shop/product-detail", {
