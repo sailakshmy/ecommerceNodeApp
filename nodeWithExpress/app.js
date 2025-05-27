287cd5baad124f6ac811f296d8d9d0925d9f74e2
Sai <sailakshmy94@gmail.com>
2025-05-27 07:11:13 +0400

    Created the Login form along with the route and the controller

--- nodeWithExpress/app.js
+++ nodeWithExpress/app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
 const errorController = require("./controllers/error");
 const mongoConnect = require("./utils/database").mongoConnect;
 const mongoose = require("mongoose");
@@ -53,6 +55,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
