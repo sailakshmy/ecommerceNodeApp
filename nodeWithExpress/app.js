96059e11064a2bb6b0d7fd02d4751baeba9cff71
Sai <sailakshmy94@gmail.com>
2025-06-01 20:22:59 +0400

    Configured multer to adjust filename and filepath

--- nodeWithExpress/app.js
+++ nodeWithExpress/app.js
@@ -37,6 +37,15 @@ const store = new MongoDbStore({
   collection: "sessions",
 });
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${new Date().toISOString()}-${file.originalname}`);
+  },
+});
+
 const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
@@ -44,7 +53,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: "images" }).single("image"));
+app.use(multer({ storage: fileStorage }).single("image"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
